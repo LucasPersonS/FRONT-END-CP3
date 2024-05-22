dd9ea0450e9379963a79e1e20198c1ad15e6e039
Lucas <lservollo@gmail.com>
2024-05-22 01:50:31 -0300

    Estilizando e arrumando pastas.
    Co-authored-by: JPBenito2006 <JPBenito2006@users.noreply.github.com>
    Co-authored-by: Vinicius Riberio Nery Costa <VN740@users.noreply.github.com>

--- HTML/JS/script.js
+++ HTML/JS/script.js
@@ -1,5 +1,5 @@
 let listaUsuarios = [
-    {txtNm : "Jeferso", txtCpf : "1234567890", rdGen: "m", txtEmail:"jeferson@email.com", txtSenha:"123456"},
+    {txtNm : "Jeferso", txtCpf : "123456789", rdGen: "m", txtEmail:"jeferso@email.com", txtSenha:"123456"},
 ];
 
 if(localStorage.getItem("base-dados") == null) {
@@ -9,7 +9,7 @@ if(localStorage.getItem("base-dados") == null) {
 
 let iconEye = document.querySelector(".fa-eye-slash, fa-eye");
 iconEye.addEventListener('click', function(){
-
+    
     let inputSenha = document.querySelector("#idSenha");
 
     if(this.className == "fa-regular fa-eye-slash" ){
@@ -20,13 +20,14 @@ iconEye.addEventListener('click', function(){
         inputSenha.setAttribute("type","password");
     }
 });
+
 function validaCampos(input1,input2) {
 
     const listaUsuarios = JSON.parse(localStorage.getItem("base-dados"))
     console.log(listaUsuarios);
-
+   
     const elMsg = document.querySelector(".valida");
-
+    
     for (let x = 0; x < listaUsuarios.length; x++) {
         if((listaUsuarios[x].txtEmail == input1.value) && (listaUsuarios[x].txtSenha == input2.value)){
 
@@ -51,22 +52,26 @@ function validaCampos(input1,input2) {
 
     elMsg.setAttribute("class","erro");
     elMsg.innerText = "Nome de usuário ou senha incorretos!";
-
+   
     return false;
 }
 
+
+//Recuperando o botão de login
 const btnLogin = document.querySelector("#login");
-
+//Recuperando o elemento dialog
  const modal = document.querySelector("dialog");
-
+ //Atrelando um evento ao botão
  btnLogin.addEventListener("click", function(e){
 
+    // console.log(e.target.innerText);
     modal.showModal();
      
  });
 
+ //Recuperando o botão de login
 const btnClose = document.querySelector("#close");
-
+ //Atrelando um evento ao botão
 btnClose.addEventListener("click", function(e){
     modal.close();
  });
