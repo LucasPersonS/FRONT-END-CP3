a8bd690b5f749de570d2c184b1951fb496bf2aab
Lucas <lservollo@gmail.com>
2024-05-22 01:30:07 -0300

    Java Script Completo.

--- script.js
+++ script.js
@@ -20,3 +20,53 @@ iconEye.addEventListener('click', function(){
         inputSenha.setAttribute("type","password");
     }
 });
+function validaCampos(input1,input2) {
+
+    const listaUsuarios = JSON.parse(localStorage.getItem("base-dados"))
+    console.log(listaUsuarios);
+
+    const elMsg = document.querySelector(".valida");
+
+    for (let x = 0; x < listaUsuarios.length; x++) {
+        if((listaUsuarios[x].txtEmail == input1.value) && (listaUsuarios[x].txtSenha == input2.value)){
+
+            //Criando um objeto no localStorage
+            localStorage.setItem("usuario-logado", JSON.stringify(listaUsuarios[x]));
+
+            const userToken = Math.random().toString(16).substring(2, 5)+Math.random().toString(16).substring(2, 5);
+
+            sessionStorage.setItem("token", userToken);
+           //Atribuir uma nova classe ao elemento de msg do usuario. 
+           elMsg.setAttribute("class","sucesso");
+           elMsg.innerText = "Login realizado com SUCESSSO!";
+
+           setTimeout(()=>{
+                elMsg.setAttribute("class","valida");
+                elMsg.innerText = "logado";
+                window.location.href = "/HTML/logado.html";
+           },3000);
+            return false; 
+        }
+    }
+
+    elMsg.setAttribute("class","erro");
+    elMsg.innerText = "Nome de usuário ou senha incorretos!";
+
+    return false;
+}
+
+const btnLogin = document.querySelector("#login");
+
+ const modal = document.querySelector("dialog");
+
+ btnLogin.addEventListener("click", function(e){
+
+    modal.showModal();
+     
+ });
+
+const btnClose = document.querySelector("#close");
+
+btnClose.addEventListener("click", function(e){
+    modal.close();
+ });
